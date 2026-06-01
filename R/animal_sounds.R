58f500e6912247b862753a4a2e20fead4591176c
smiregorio <smirnov1egor@gmail.com>
2026-06-01 12:38:43 +0200

    experiment, second day

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,15 @@
+#' Put your title here
+#'
+#' Description
+#'
+#' @param animal A string
+#' @param sound A string
+#'
+#' @returns A string with the animal and sound
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog","woof")
 animal_sounds <- function(animal,sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
